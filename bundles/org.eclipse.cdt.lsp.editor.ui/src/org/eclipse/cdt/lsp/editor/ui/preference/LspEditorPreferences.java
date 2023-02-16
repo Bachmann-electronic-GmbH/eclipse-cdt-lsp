e3b18b9c900db4c63be4515e9e0a9f8102679a0a
Jonah Graham <jonah@kichwacoders.com>
2023-02-16 11:55:34 -0500

    Apply CDT coding standards
    This was done by by running
    org.eclipse.cdt/releng/scripts/do_all_code_cleanups.sh
    and then updating classpaths in Eclipse to account
    for this repo not doing Java 17 yet.

--- bundles/org.eclipse.cdt.lsp.editor.ui/src/org/eclipse/cdt/lsp/editor/ui/preference/LspEditorPreferences.java
+++ bundles/org.eclipse.cdt.lsp.editor.ui/src/org/eclipse/cdt/lsp/editor/ui/preference/LspEditorPreferences.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2023 Bachmann electronic GmbH and others.
- * 
+ *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Contributors:
  * Gesa Hentschke (Bachmann electronic GmbH) - initial implementation
  *******************************************************************************/
@@ -17,7 +17,7 @@ import org.eclipse.cdt.lsp.editor.ui.LspEditorUiMessages;
 import org.eclipse.core.runtime.preferences.PreferenceMetadata;
 
 public final class LspEditorPreferences {
-	
+
 	public static PreferenceMetadata<Boolean> getPreferenceMetadata() {
 		return new PreferenceMetadata<>(Boolean.class, "prefer_lsp_editor", false, //$NON-NLS-1$
 				LspEditorUiMessages.LspEditorPreferencePage_preferLspEditor,
